9ff5962aca5f6b60daa7aeba8994d15a14ec3dd2
Krzysztof Domino <kdomino@iitis.pl>
2022-06-03 14:36:23 +0200

    improve documentation of input

--- test/test_rescheduling_examples.py
+++ test/test_rescheduling_examples.py
@@ -2,19 +2,46 @@ import numpy as np
 from railway_solvers import make_Q, energy
 
 
-# trains_timing input is extected to be in the following form of dict of dicts
-#  taus are given as
-# taus = {"pass" : {"j_s_si" : τ^pass(j,s,s1), ...},
-# "headway" : {"j_j1_s_s1" : τ^headway(j,j1,s,s1)  .... },
-# "stop": {"j_s_None" : τ^stop(j,s)}, "res": τ^res}
-# τ^res is just one for all situations, it may need to be extended
+############## general input ###########
 
-# train schedule if available (train can not leave before schedule)
-# schedule = {"j_s" : t_schedule(j,s_out), ... }
 
-# trains_timing = {"tau": taus, "schedule" : schedule,
+# taus =
+#{"pass" : {"j_s_s1" : τ^pass(j,s,s1), ...} ==>  passing time of j s -> s1
+# "headway" : {"j_j1_s_s1" : τ^headway(j,j1,s,s1),  .... } ==> both j and j1 goes s->s1
+# minimal time j1 must wait at s after j leaving s
+# "stop": {"j_s" : τ^stop(j,s)}, ....  }  ==> minimal stopping time of j at s
+#"res": τ^res} ==> switch occupation time, set globally
+
+# headway, pass, block - Fig. 2
+# prep, res. - Fig 3.
+
+
+# trains_timing = {"tau": taus,
 #              "initial_conditions" : {"j_s" : t(j,s_out), ...},
 #              "penalty_weights" : {"j_s" : w(j,s), ...}}
+
+
+# w(j,s) => penalty factor for j leaving s later than unaviodlable delay.
+
+
+#to ensure train can not leave befor the due time, there also ben be added:
+# "schedule": schedule = {"j_s" : t_schedule(j,s_out), ... }
+
+
+############  trains path #########
+# trains_paths = {
+    #"Paths": {"j": ["s1", "s2", "s3", ...] ...} => path of j marked by stations
+    # "J": ["j", "j1", "j2", ...] => se of all trains
+    # "Jd": {"s": {"s1": [["j", "j1", "j2",...], ["i", "i1",...]]...}...} =>
+    # from s to s1 goes "j", "j1", "j2",... on one track and "i", "i1",.. on another track
+    #  "Josingle": {("s", "s1"): [[j, j1]]} => j goes s -> s1 while j1 goes s1 -> s on the same (single) track (pairs only)
+    # "Jround": {"s": [[j, j1], [i, i1]] ...} => at s j terminates
+    # and starts than as a new train j1 the same is with the pair i, i1  (pairs only)
+    # "Jtrack": {"s": [[j, j1, j2] ..] ...} => at s j, j1 dna j2 uses the same track
+    # "Jswitch": {"s": [{j: "out", j1: "in"}, ....]. ...}  => at s pair j and j1 uses the
+    # same switch while j entering, and j1 leaving
+
+
 
 
 def test_headway_two_trains():
@@ -38,7 +65,7 @@ def test_headway_two_trains():
     taus = {"pass": {"j1_A_B": 4, "j2_A_B": 8},
             "headway": {"j1_j2_A_B": 2, "j2_j1_A_B": 6},
             "stop": {"j1_B": 1, "j2_B": 1}, "res": 1}
-    trains_timing_1 = {"tau": taus,
+    trains_timing = {"tau": taus,
                  "initial_conditions": {"j1_A": 3, "j2_A": 1}, # leaving times
                  "penalty_weights": {"j1_A": 2, "j2_A": 0.5}}  # these are w_j
 
@@ -57,7 +84,7 @@ def test_headway_two_trains():
     p_qubic = 2.
     d_max = 5
 
-    Q = make_Q(d_max, p_sum, p_pair, p_qubic, trains_timing_1, trains_paths)
+    Q = make_Q(d_max, p_sum, p_pair, p_qubic, trains_timing, trains_paths)
 
     assert np.array_equal(Q, np.load("test/files/Qfile_one_way.npz")["Q"])
 
@@ -86,7 +113,7 @@ def  test_station_track_and_switches_two_trains():
     taus = {"pass": {"0_A_B": 4, "1_A_B": 4},
             "headway": {"0_1_A_B": 2, "1_0_B_A": 4},
             "stop": {"0_B": 1, "1_B": 1}, "res": 2}
-    trains_timing_2 = {"tau": taus,
+    trains_timing = {"tau": taus,
                  "initial_conditions": {"0_A": 1, "1_A": 1},
                  "penalty_weights": {"0_A": 2, "1_A": 0.5}}
 
@@ -106,7 +133,7 @@ def  test_station_track_and_switches_two_trains():
     p_qubic = 2.
     d_max = 5
 
-    Q = make_Q(d_max, p_sum, p_pair, p_qubic, trains_timing_2, trains_paths)
+    Q = make_Q(d_max, p_sum, p_pair, p_qubic, trains_timing, trains_paths)
 
     assert np.array_equal(Q, np.load("test/files/Qfile_track.npz")["Q"])
 
@@ -144,7 +171,7 @@ def test_deadlock_and_switches_two_trains():
     taus = {"pass": {"0_A_B": 4, "1_B_A": 8},
             "stop": {"0_B": 1, "1_A": 1}, "res": 1}
 
-    trains_timing_3 = {"tau": taus,
+    trains_timing = {"tau": taus,
                 "initial_conditions": {"0_A": 3, "1_B": 1},
                 "penalty_weights": {"0_A": 2, "1_B": 0.5}}
 
@@ -153,7 +180,7 @@ def test_deadlock_and_switches_two_trains():
     p_qubic = 2.
     d_max = 10
 
-    Q = make_Q(d_max, p_sum, p_pair, p_qubic, trains_timing_3, trains_paths)
+    Q = make_Q(d_max, p_sum, p_pair, p_qubic, trains_timing, trains_paths)
 
     assert np.array_equal(Q, np.load("test/files/Qfile_two_ways.npz")["Q"])
 
