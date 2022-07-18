46167b504b3b90e9eba44feb4013ae13983c4cff
Akash Kundu <kunduaku339@gmail.com>
2022-07-18 16:27:23 +0200

    plots modified

--- plot_DWave_results.py
+++ plot_DWave_results.py
@@ -27,8 +27,7 @@ list_route_full = [["enlarged", "5trains"], ["rerouted", None ]]
 for k, list_route in enumerate(list_route_full):
     fig, axes = plt.subplots(ncols=2)
     fig, ax = plt.subplots(1, 2, figsize=(5,3),sharey=True, sharex=True)
-# default setting
-    print(k)
+
     for i, route in enumerate(list_route):
         non_feasible_css = [3, 3.5, 4, 4.5]
         non_feasible_def = { 'en':[], 'cs':[] }
@@ -47,7 +46,7 @@ for k, list_route in enumerate(list_route_full):
         non_feasible_def['cs'].remove( feasible_css )
 
         with open('files/hybrid_data/Qfile_samples_sol_hybrid-anneal' +f, 'rb') as fp:
-            ground = [pickle.load(fp)[0][1]]
+            hybrid = [pickle.load(fp)[0][1]]
 
         non_feasible_css_mod = non_feasible_def['cs']
         non_feasible_ens_mod = non_feasible_def['en']
@@ -57,22 +56,34 @@ for k, list_route in enumerate(list_route_full):
         if i ==0:
             ax[i].plot(non_feasible_css_mod, non_feasible_ens_mod, 'rs', label = 'D-Wave non-feasible solution')
             if k == 0:
-                ax[i].plot(feasible_css, feasible_ens, 'rs', label = 'D-Wave solution')
-            else:
+                ax[i].plot(feasible_css, feasible_ens, 'rs')
+                ax[i].axhline(y =hybrid, color = 'g', label = 'Hybrid solver')
+                if route == 'enlarged':
+                    ax[i].axhline(y =-14.4, linestyle = '--', color = 'g',label = 'Ground state')
+                else:
+                    ax[i].axhline(y =-21.49,  linestyle = '--',  color = 'g',label = 'Ground state')
+            elif k ==1:
                 ax[i].plot(feasible_css, feasible_ens, 'go', label = 'D-Wave feasible solution')
+                ax[i].axhline(y =hybrid, color = 'g', label = 'Ground state and Hybrid solver')
             ax[i].set_xticks(non_feasible_css)
             ax[i].set_title(f'{grafitti(route)}')
-            ax[i].axhline(y =ground, color = 'g', label = 'Ground state and hybrid solver')
+            
         else:
             ax[i].plot(non_feasible_css_mod, non_feasible_ens_mod, 'rs')
             if k == 0:
+                ax[i].plot(feasible_css, feasible_ens, 'rs')
+                ax[i].axhline(y =hybrid, color = 'g')
+                if route == 'enlarged':
+                    ax[i].axhline(y =-14.4, linestyle = '--', color = 'g')
+                else:
+                    ax[i].axhline(y =-21.49,  linestyle = '--',  color = 'g')
                 ax[i].plot(feasible_css, feasible_ens, 'rs')
             elif k ==1:
                 ax[i].plot(feasible_css, feasible_ens, 'go')
             # ax[i].plot(feasible_css, feasible_ens, 'rs')
             ax[i].set_xticks(non_feasible_css)
             ax[i].set_title(f'{grafitti(route)}')
-            ax[i].axhline(y =ground, color = 'g')
+            ax[i].axhline(y =hybrid, color = 'g')
         ax[i].set_xlabel('Chain strength')
 
 
@@ -85,5 +96,3 @@ for k, list_route in enumerate(list_route_full):
         fig.savefig("plots/DW_trains_larger.pdf")
     elif k ==1:
         fig.savefig("plots/DW_trains.pdf")
-
-
